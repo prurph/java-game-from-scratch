8455f2d0364912608c677fbf006a4ec32f953920
Prescott Murphy <prurph@gmail.com>
2015-05-22 08:23:55 -0400

    Complete Episode 18: Controlling the Map.

--- src/tt/presco/rain/input/Keyboard.java
+++ src/tt/presco/rain/input/Keyboard.java
@@ -5,7 +5,7 @@ import java.awt.event.KeyListener;
 
 public class Keyboard implements KeyListener {
 
-    private boolean[] keys = new boolean[120];
+    private boolean[] keys = new boolean[158];
     public boolean up, down, left, right;
 
     public void update() {
@@ -14,7 +14,6 @@ public class Keyboard implements KeyListener {
         left = keys[KeyEvent.VK_LEFT] || keys[KeyEvent.VK_A];
         right = keys[KeyEvent.VK_RIGHT] || keys[KeyEvent.VK_D];
 
-        System.out.println(up);
     }
 
     @Override
