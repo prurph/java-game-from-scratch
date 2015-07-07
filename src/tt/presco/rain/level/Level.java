26249180fc531a51506fb0d1f9fca21bf052778d
Prescott Murphy <prurph@gmail.com>
2015-07-06 20:11:11 -0400

    Complete Episode 24: Level Class Overview.

---
+++ src/tt/presco/rain/level/Level.java
@@ -0,0 +1,32 @@
+package tt.presco.rain.level;
+
+import tt.presco.rain.graphics.Screen;
+
+public class Level {
+
+    private int width, height;
+    private int[] tiles;
+
+    public Level(int width, int height) {
+        this.width = width;
+        this.height = height;
+        tiles = new int[width * height];
+        generateLevel();
+    }
+
+    public Level(String path) {
+        loadLevel(path);
+    }
+
+    private void generateLevel() {
+    }
+
+    private void loadLevel(String path) {
+    }
+
+    public void update() {
+    }
+
+    public void render(int xScroll, int yScroll, Screen screen) {
+    }
+}
